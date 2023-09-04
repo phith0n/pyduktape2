56e91045da5dad6093ff0e16f79df18dc11ca61e
phith0n <phith0n.ph2f@gmail.com>
2023-09-04 17:03:42 +0800

    pre-release 0.4.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extensions = [
 
 setup(
     name='pyduktape2',
-    version='0.4.4',
+    version='0.4.5',
     author='Stefano Dissegna',
     description='Python integration for the Duktape Javascript interpreter',
     long_description=long_description,
